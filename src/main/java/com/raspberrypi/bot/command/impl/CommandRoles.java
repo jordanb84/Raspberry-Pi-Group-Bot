488f568333f74f6c8d493e5974486ac07556cee0
Megalegacy98 <megalegacy98@gmail.com>
2016-11-03 21:21:13 -0700

    Added frame for role change command.

---
+++ src/main/java/com/raspberrypi/bot/command/impl/CommandRoles.java
@@ -0,0 +1,17 @@
+package com.raspberrypi.bot.command.impl;
+import com.raspberrypi.bot.command.*;
+
+import de.btobastian.javacord.DiscordAPI;
+import de.btobastian.javacord.entities.message.Message;
+
+public class CommandRoles extends Command {
+	public CommandRoles(){
+		super("roleadd", new String[]{}, "Adds you to a specifc role.");
+	}
+	
+	@Override
+	public void onUse(DiscordAPI api, String[] arguments, Message callMessage) throws Exception {
+		
+	
+	}
+}
