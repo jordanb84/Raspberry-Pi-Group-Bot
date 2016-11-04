ef57e68a06c2efa56c31794b5d6013caa4ff86a5
Megalegacy98 <megalegacy98@gmail.com>
2016-11-03 21:41:16 -0700

    Test commit

--- src/main/java/com/raspberrypi/bot/command/impl/CommandRoles.java
+++ src/main/java/com/raspberrypi/bot/command/impl/CommandRoles.java
@@ -1,17 +1,18 @@
 package com.raspberrypi.bot.command.impl;
-import com.raspberrypi.bot.command.*;
-
+import com.raspberrypi.bot.command.Command;
 import de.btobastian.javacord.DiscordAPI;
 import de.btobastian.javacord.entities.message.Message;
 
 public class CommandRoles extends Command {
 	public CommandRoles(){
-		super("roleadd", new String[]{}, "Adds you to a specifc role.");
+		super("roleadd", new String[] {}, "Adds you to a specifc role.");
 	}
 	
 	@Override
-	public void onUse(DiscordAPI api, String[] arguments, Message callMessage) throws Exception {
+	public void onUse(DiscordAPI api, String[] arguments, Message callMessage) throws Exception{
+		String b = callMessage.getChannelReceiver().getName();
 		
+		callMessage.reply(b);
 	
 	}
 }
